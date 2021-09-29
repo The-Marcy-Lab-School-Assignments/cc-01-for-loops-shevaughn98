9d8caa8edec44701620ab943260d3e547a8d5c01
EC2 Default User <ec2-user@ip-172-31-26-46.us-east-2.compute.internal>
2021-09-29 00:59:42 +0000

    funtions 1-3

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 //1
 function countToFifty(){
-  
+for (let i = 1; 1 <= 50; i++)
+console.log(i)
 }
 
 //2
 function countFromOne(){
+    for(let i = 0; 0 <= 5; i++)
+    console.log(i)
   
 }
 
 //3
 function threeAndFiveInFifty(){
-  
-}
+  for(let i = 1; 1 <= 51; i++){
+      console.log(i)
+  if (i % 3 === 0){
+      console.log(i)
+      
+  }
 
+}
 //4
-function threeAndFive(){
+//function threeAndFive(){
   
 }
 
-
+threeAndFiveInFifty()
